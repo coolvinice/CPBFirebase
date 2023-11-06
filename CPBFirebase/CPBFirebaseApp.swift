c975e2c5231496dbafc7e6862e316f6543700598
Calvin Rice <ramen.calvin@yahoo.com>
2023-11-06 10:43:21 -0600

    Configured Firebase

--- CPBFirebase/CPBFirebaseApp.swift
+++ CPBFirebase/CPBFirebaseApp.swift
@@ -6,9 +6,13 @@
 //
 
 import SwiftUI
+import FirebaseCore
 
 @main
 struct CPBFirebaseApp: App {
+    init() {
+        FirebaseApp.configure()
+    }
     var body: some Scene {
         WindowGroup {
             ContentView()
