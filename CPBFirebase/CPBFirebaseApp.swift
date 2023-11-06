37822bb1bc35d4f27153dc15109c3cd57e836202
Calvin Rice <ramen.calvin@yahoo.com>
2023-11-06 10:27:47 -0600

    Initial Commit

---
+++ CPBFirebase/CPBFirebaseApp.swift
@@ -0,0 +1,17 @@
+//
+//  CPBFirebaseApp.swift
+//  CPBFirebase
+//
+//  Created by Calvin Rice on 11/6/23.
+//
+
+import SwiftUI
+
+@main
+struct CPBFirebaseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CPBFirebase/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CPBFirebase
+//
+//  Created by Calvin Rice on 11/6/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
